cef6180a95b2395a02afb54b8c467adba065e0f9
Sarax.srx <80204080+Sarax857@users.noreply.github.com>
2021-11-17 15:52:44 +0300

    wordlist_gen.py

---
+++ wordlist_gen.py
@@ -0,0 +1,64 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============ RESTART: C:\Users\ADMIN\OneDrive\Masaüstü\wordlistgn.py ===========
+
+                              
+######################################################
+                By                            
+######################################################
+
+    Suraj Singh
+     
+     
+    surajsinghbisht054@gmail.com
+    http://www.bitforestinfo.com/
+
+     
+######################################################
+
+#######################################################################
+# High Speed Wordlist Generator : Compitable To All Operating Systems #
+#######################################################################
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+usages Example:>>
+        Please Enter Here All Word for combination :>> ABCabc123';./
+        please enter here lengh of words :>>4
+        please enetr here name of Wordlist :>>>pass.txt
+**********************************************************************
+**********************************************************************
+
+
+[+] Please Enter Here All Word for combination :>> abcdg
+[+] Please Enter Minimum Lenth Of Words  : >>  2
+[+] Please Enter Maximum Lenth Of Words  : >>  4
+[+] Please Enetr Here Name Of Wordlist File :>>> deneme.txt
+[+] Numbers Of Total Lines :  775
+[+] Are You Ready ?[Press Enter]
+
+
++++++++++++++++++++++++++ Please Wait ++++++++++++++++++++++++++++++++++++++++
+ 
+
+
+40.0 percent 
+60.0 percent 
+80.0 percent 
+Done Sucessfully
+
++++++++++++++++++++++++++ Process Report +++++++++++++++++++++++++++++++++++++
+
+	 [+] Process Started                      :    Wed Nov 17 15:50:35 2021
+	 [+] Process Completed                    :    Wed Nov 17 15:50:35 2021
+	 [+] Total Time Consumed                  :    0.02399611473083496 second
+	 [+] Rate Of Words Generating Per Seconds :    32296.895088775622
+
++++++++++++++++++++++++++ Please Wait ++++++++++++++++++++++++++++++++++++++++
+
+	***************************************************
+	*       Successfully created thanks for using     *
+	***************************************************
+
+
+
+[*] Press Enter For Exit
